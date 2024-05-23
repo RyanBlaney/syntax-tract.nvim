98a740350c828aa37dc52d72d28f65407706e4cb
RyanBlaney <ryanblaney2004@gmail.com>
2024-05-23 16:42:07 -0700

    Allowed for lazy loading

--- lua/syntax-tract/init.lua
+++ lua/syntax-tract/init.lua
@@ -1,55 +1,63 @@
-require('syntax-tract').setup({
-  languages = {
-    cpp = {
-      words = {
-        ["std::"] = "⊇",
-        ["\\n"] = "⏎",
-        ["%(int%)"] = "⚙",
-        ["%(unsigned int%)"] = "⚙",
-        ["%(char%)"] = "⚙",
-        ["%(float%)"] = "⚙",
-        ["%(double%)"] = "⚙",
-        ["%(long long%)"] = "⚙",
-        ["%(unsigned long long%)"] = "⚙",
-        ["%(short%)"] = "⚙",
-        ["%(unsigned short%)"] = "⚙",
-        ["%(long%)"] = "⚙",
-        ["%(unsigned long%)"] = "⚙",
-        ["%(bool%)"] = "⚙",
-        ["%(wchar_t%)"] = "⚙",
-        ["%(unsigned char%)"] = "⚙",
-        ["%(signed char%)"] = "⚙",
-        ["%(void%*)"] = "⚙",
-        ["%(size_t%)"] = "⚙",
-        ["%(ptrdiff_t%)"] = "⚙",
-        ["%(intptr_t%)"] = "⚙",
-        ["%(uintptr_t%)"] = "⚙",
-        ["%(std::string%)"] = "⚙",
-        ["%(std::wstring%)"] = "⚙",
-        ["static_cast<int>"] = "⚙",
-        ["static_cast<unsigned int>"] = "⚙",
-        ["static_cast<char>"] = "⚙",
-        ["static_cast<float>"] = "⚙",
-        ["static_cast<double>"] = "⚙",
-        ["static_cast<long long>"] = "⚙",
-        ["static_cast<unsigned long long>"] = "⚙",
-        ["static_cast<short>"] = "⚙",
-        ["static_cast<unsigned short>"] = "⚙",
-        ["static_cast<long>"] = "⚙",
-        ["static_cast<unsigned long>"] = "⚙",
-        ["static_cast<bool>"] = "⚙",
-        ["static_cast<wchar_t>"] = "⚙",
-        ["static_cast<unsigned char>"] = "⚙",
-        ["static_cast<signed char>"] = "⚙",
-        ["static_cast<void*>"] = "⚙",
-        ["static_cast<size_t>"] = "⚙",
-        ["static_cast<ptrdiff_t>"] = "⚙",
-        ["static_cast<intptr_t>"] = "⚙",
-        ["static_cast<uintptr_t>"] = "⚙",
-        ["static_cast<std::string>"] = "⚙",
-        ["static_cast<std::wstring>"] = "⚙",
+local M = {}
+
+M.setup = function(opts)
+  -- Default options
+  M.defaults = {
+    languages = {
+      cpp = {
+        words = {},
+        color = "#ff8a8a",
       },
-      color = "#ff8a8a",
     },
-  },
-})
+  }
+
+  -- Merge user options with default options
+  M.opts = vim.tbl_deep_extend("force", M.defaults, opts or {})
+
+  -- Setup highlight group
+  vim.cmd(string.format("highlight SyntaxTractConcealed ctermfg=LightRed guifg=%s", M.opts.languages.cpp.color))
+
+  -- Function to conceal words
+  local function conceal_words(bufnr, lang_opts)
+    local ns_id = vim.api.nvim_create_namespace("syntax_tract")
+    local hl_group = "SyntaxTractConcealed"
+    local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+    for linenr, line in ipairs(lines) do
+      for word, symbol in pairs(lang_opts.words) do
+        local start_pos, end_pos = string.find(line, word)
+        while start_pos do
+          vim.api.nvim_buf_set_extmark(bufnr, ns_id, linenr-1, start_pos-1, {
+            end_col = end_pos,
+            conceal = symbol,
+            hl_group = hl_group,
+          })
+          start_pos, end_pos = string.find(line, word, end_pos + 1)
+        end
+      end
+    end
+  end
+
+  -- Function to remove concealment on the current line
+  local function reveal_line(bufnr, line_nr)
+    local ns_id = vim.api.nvim_create_namespace("syntax_tract")
+    vim.api.nvim_buf_clear_namespace(bufnr, ns_id, line_nr, line_nr + 1)
+  end
+
+  -- Setup autocommands for each language
+  for lang, lang_opts in pairs(M.opts.languages) do
+    vim.cmd(string.format([[
+      augroup SyntaxTract_%s
+        autocmd!
+        autocmd BufReadPost,BufWritePost *.%s lua require('syntax_tract').conceal_words(0, '%s')
+        autocmd CursorMoved *.%s lua require('syntax_tract').reveal_line(0, vim.fn.line('.') - 1)
+        autocmd CursorMoved *.%s lua require('syntax_tract').conceal_words(0, '%s')
+      augroup END
+    ]], lang, lang, lang, lang, lang, lang))
+  end
+
+  -- Expose functions
+  M.conceal_words = conceal_words
+  M.reveal_line = reveal_line
+end
+
+return M
