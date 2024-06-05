21618738461b4c40fd339388a7876cebc3632145
RyanBlaney <ryanblaney2004@gmail.com>
2024-06-04 18:48:30 -0700

    Fixed overflow bugs

--- lua/syntax-tract/init.lua
+++ lua/syntax-tract/init.lua
@@ -53,7 +53,7 @@ M.setup = function(opts)
             local remaining_text = symbol_length
             local end_col = start_pos - 1 + word_length
             if symbol_length > word_length then
-              end_col = end_col - remaining_text - 1
+              end_col = end_col - remaining_text + 1
             end
 
             vim.api.nvim_buf_set_extmark(bufnr, ns_id, linenr - 1, start_pos - 1, {
