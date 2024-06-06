a61731fd784451e8888ca380baa5ed2fc0d980ba
RyanBlaney <ryanblaney2004@gmail.com>
2024-06-05 21:20:00 -0700

    Fixed out of bounds error

--- lua/syntax-tract/init.lua
+++ lua/syntax-tract/init.lua
@@ -1,4 +1,3 @@
-
 local M = {}
 local defaults = require('syntax-tract.defaults').defaults
 
@@ -47,12 +46,20 @@ M.setup = function(opts)
 
           if not already_replaced then
             local end_col = start_pos - 1 + word_length
+            local remaining_start_pos = end_pos
 
+            -- Adjust remaining text position if symbol is longer than the word
             if symbol_length > word_length then
-              end_col = end_col - symbol_length
+              end_col = start_pos - 1 + symbol_length
+              local padding_length = symbol_length - word_length
+              local padding = string.rep(" ", padding_length)
+              remaining_start_pos = start_pos - 1 + symbol_length
+              vim.api.nvim_buf_set_extmark(bufnr, ns_id, linenr - 1, remaining_start_pos, {
+                end_col = #line,
+                virt_text = {{padding, "Normal"}},
+                virt_text_pos = "inline",
+              })
             end
-
-            end_col = math.min(end_col, #line)
 
             vim.api.nvim_buf_set_extmark(bufnr, ns_id, linenr - 1, start_pos - 1, {
               end_col = end_col,
@@ -61,20 +68,6 @@ M.setup = function(opts)
               virt_text_pos = "overlay",
               hl_group = hl_group,
             })
-
-            -- Adjust remaining text position if symbol is longer than the word
-            if symbol_length > word_length then
-              local padding_length = symbol_length - word_length
-              local padding = string.rep(" ", padding_length)
-              local remaining_text = line:sub(end_pos + 1)
-              local remaining_start_pos = start_pos - 1 + symbol_length
-              vim.api.nvim_buf_set_extmark(bufnr, ns_id, linenr - 1, remaining_start_pos, {
-                end_col = math.min(remaining_start_pos + #remaining_text, #line),
-                virt_text = {{padding, "Normal"}},
-                virt_text_pos = "inline",
-                -- hl_group = hl_group,
-              })
-            end
           end
 
           start_pos, end_pos = string.find(line, escaped_word, end_pos + 1)
@@ -174,3 +167,4 @@ M.setup = function(opts)
 end
 
 return M
+
